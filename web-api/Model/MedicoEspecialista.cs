9d11ea82bb8cf20cbb7e0a214dcaa6f460e9fc26
carloscalil <calil.carlosf@gmail.com>
2023-07-17 12:10:27 -0300

    primeito commit

---
+++ web-api/Model/MedicoEspecialista.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace web_api.Model
+{
+    public class MedicoEspecialista: Models.Medico
+    {
+    }
+}
